4cb037182d879d1fbdb18f1633f269228c18e280
autograph <ldragon837@email.com>
2026-03-31 18:50:03 +0300

    made models

--- api/models.py
+++ api/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-
+PRIORITY_CHOICES = [('low', 'Low'), ('medium', 'Medium'), ('high','High')]
 class Task(models.Model):
     title = models.CharField(max_length = 255)
-    completed = models.BooleanField()
-    created_at = auto_now_add=True
-    priority = PRIORITY_CHOICES
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length = 6, choices = PRIORITY_CHOICES)
