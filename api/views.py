99d24d7ab928d273cab1b79393de60f31b286ae9
autograph <ldragon837@email.com>
2026-04-01 20:05:24 +0300

    start views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+tasks = Task.objects.all()
+
+result = []
+for task in tasks:
+    result.append({
+    "title": task.title,
+    "created_at": task.created_at,
+    "completed": task.completed,
+    "priority": task.priority
+})
+
