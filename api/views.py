3359846a7c6f9738cff176fe2b658ef12f67effa
autograph <ldragon837@email.com>
2026-04-02 18:56:21 +0300

    made views

--- api/views.py
+++ api/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from .models import Task
+from django.http import JsonResponse
 
-tasks = Task.objects.all()
 
-result = []
-for task in tasks:
-    result.append({
-    "title": task.title,
-    "created_at": task.created_at,
-    "completed": task.completed,
-    "priority": task.priority
-})
+
+def get_tasks(request):
+    tasks = Task.objects.all()
+    data = decode(tasks)
+    return JsonResponse(data,safe=False)
+
+
+def decode(tasks):
+    result = []
+
+    for task in tasks:
+        result.append({
+        "title": task.title,
+        "created_at": task.created_at,
+        "completed": task.completed,
+        "priority": task.priority
+    })
+    return result
 
