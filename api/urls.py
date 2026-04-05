ed2834ad2e48343b0e465e548b52fe6e72f206fc
autograph <ldragon837@gmail.com>
2026-04-05 19:24:14 +0300

    postrequest

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
-from .views import get_tasks
+from .views import get_tasks, post_task
 from django.urls import path
 urlpatterns = [
     path('tasks/',get_tasks),
+    path('post_task/',post_task),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Task
 from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 
 
@@ -21,4 +23,17 @@ def decode(tasks):
         "priority": task.priority
     })
     return result
+@csrf_exempt
+def post_task(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        Task.objects.create(
+            title = data["title"],
+            priority = data["priority"]
+        )
+        return JsonResponse({"status":"ok"})
+        
+    else:
+        return(JsonResponse({"error": "Invalid method"}))
+
 
