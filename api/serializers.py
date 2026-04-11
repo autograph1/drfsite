f95c5e52da63438c0bd0f46d73ca4978584aea7c
autograph1 <ldragon837@gmail.com>
2026-04-11 22:39:39 +0300

    first serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
-from .models import PRIORITY_CHOICES
+from .models import PRIORITY_CHOICES, Task
 
 class TaskSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     completed = serializers.BooleanField()
     priority = serializers.ChoiceField(choices=PRIORITY_CHOICES)
     created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return Task.objects.create(**validated_data)
