f405e2c0ca2350d020b6e839ce486914faaae437
autograph <ldragon837@gmail.com>
2026-04-13 18:55:33 +0300

    k

--- api/views.py
+++ api/views.py
@@ -5,29 +5,19 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import TaskSerializer
 
-def decode(tasks):
-    result = []
-
-    for task in tasks:
-        result.append({
-        "title": task.title,
-        "created_at": task.created_at,
-        "completed": task.completed,
-        "priority": task.priority
-    })
-    return result
 @csrf_exempt
 def tasks(request):
     if request.method == "GET":
         tasks = Task.objects.all()
-        data = decode(tasks)
-        return JsonResponse(data,safe=False)
+        serializer = TaskSerializer(tasks, many=True)
+        return JsonResponse(serializer.data, safe=False)
+        
     elif request.method == "POST":
         data = json.loads(request.body)
         serializer = TaskSerializer(data=data) 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status = 200)
+            return JsonResponse(serializer.data, status = 201)
         else:
             return JsonResponse(serializer.errors, status = 400)
 
