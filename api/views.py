017390636e1df30f3ca8458e26eca041a47c7dba
autograph <ldragon837@gmail.com>
2026-04-14 17:52:15 +0300

    deleted

--- api/views.py
+++ api/views.py
@@ -24,6 +24,42 @@ class TaskListView(APIView):
 @csrf_exempt
 
 class TaskDetailView(APIView):
+    def get(self,request,id):
+        try:
+            task = Task.objects.get(id=id)
+        except Task.DoesNotExis:
+            return Response("error":"not found",status=404)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+    
+    def put(self,request,id):
+        try:
+            task = Task.objects.get(id=id)
+        except Task.DoesNotExist:
+            return Response({"error":"not found"}, status=404)
+        
+        serializer = TaskSerializer(task,data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=200)
+        
+        return Response(serializer.errors,status=400)
+    
+    def delete(self,request,id):
+        try:
+            task = Task.objects.get(id=id)
+        except Task.DoesNotExist:
+            return Response({"error":"not found"}, status=404)
+        
+        task.delete()
+        return Response(status=204)
+        
+        
+
+
+
+
 
  
 
