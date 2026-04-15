46228b54c115d49fba643d5fbc9cfad65d6916da
autograph1 <ldragon837@gmail.com>
2026-04-16 00:09:10 +0300

    filter

--- api/views.py
+++ api/views.py
@@ -3,12 +3,23 @@ from .serializers import TaskSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
 class TaskListView(APIView):
-    def get(self,request):
+    def get(self, request):
         tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks,many=True) 
-        return Response(serializer.data,status=200)
+
+        priority = request.GET.get('priority')
+        if priority:
+            tasks = tasks.filter(priority=priority)
+
+        completed = request.GET.get('completed')
+        if completed:
+            if completed.lower() == 'true':
+                tasks = tasks.filter(completed=True)
+            elif completed.lower() == 'false':
+                tasks = tasks.filter(completed=False)
+
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
     
     def post(self,request):
         serializer = TaskSerializer(data=request.data) 
@@ -17,7 +28,6 @@ class TaskListView(APIView):
             return Response(serializer.data,status=201)
         else:
             return Response(serializer.errors,status=400)
-
 
 
 class TaskDetailView(APIView):
