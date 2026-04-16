36351585ef0b9489143f7e2a242a00f3eedae4b7
autograph1 <ldragon837@gmail.com>
2026-04-16 22:34:17 +0300

    page

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'django-filters',
+    'django_filters',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
