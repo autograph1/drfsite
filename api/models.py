cfe196f065cf18d7f5e0a47e2dd31889895b85e2
autograph1 <ldragon837@gmail.com>
2026-04-17 20:31:52 +0300

    auth

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 PRIORITY_CHOICES = [('low', 'Low'), ('medium', 'Medium'), ('high','High')]
@@ -7,3 +8,4 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     priority = models.CharField(max_length = 6, choices = PRIORITY_CHOICES)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -3,8 +3,12 @@ from .models import Task
 from .serializers import TaskSerializer
 
 class TaskListView(ListCreateAPIView):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+    def perform_create(self,serializer):
+        serializer.save(owner=self.request.user)
+
 
 class TaskDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
