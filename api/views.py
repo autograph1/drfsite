b9e01092b699a1bb2a2e50e18416905ace3ebfaa
autograph <ldragon837@gmail.com>
2026-04-26 13:14:10 +0300

    made jwt

--- api/views.py
+++ api/views.py
@@ -3,12 +3,20 @@ from .models import Task
 from rest_framework.permissions import IsAuthenticated
 from .serializers import TaskSerializer
 from .permissions import IsOwner
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class TaskListView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = TaskSerializer
     def get_queryset(self):
         return Task.objects.filter(owner=self.request.user)
+    
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
+
+    filterset_fields = ['completed', 'priority']
+    search_fields = ['title']
+    ordering_fields = ['created_at', 'priority'] 
     def perform_create(self,serializer):
         serializer.save(owner=self.request.user)
 
