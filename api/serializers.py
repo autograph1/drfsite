098c682d396f62c7c7544c09c8d68cd2e8738b2d
autograph1 <ldragon837@gmail.com>
2026-05-01 22:20:50 +0300

    fdkj

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,8 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Too short")
+        return value
